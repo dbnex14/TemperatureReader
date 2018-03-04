d26475cdbf819cafaa99db1daa29575b7388514e
dino buljubasic <dbnex14@gmail.com>
2018-03-04 14:36:23 -0800

    init

---
+++ TemperatureReader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TemperatureReader
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
