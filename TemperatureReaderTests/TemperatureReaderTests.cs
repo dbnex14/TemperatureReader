b7cf27d769cdb03a198afad31ee7761b072433ff
dino buljubasic <dbnex14@gmail.com>
2018-03-04 17:45:16 -0800

    Adding tests for ThermometerReader.

---
+++ TemperatureReaderTests/TemperatureReaderTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using TemperatureReader;
+
+namespace TemperatureReaderTests
+{
+    [TestFixture]
+    public class TemperatureReaderTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            IThermometerReader reader = new ThermometerReader();
+        }
+    }
+}
